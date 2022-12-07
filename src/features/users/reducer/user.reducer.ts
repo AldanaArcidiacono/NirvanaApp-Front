e657d9dc8a4bd0af323b7b2992ec42aeb08406a5
AldanaArcidiacono <aldana.arcidiacono@gmail.com>
2022-12-07 20:16:16 +0100

    ✨ Add reducer and actions

--- src/features/users/reducer/user.reducer.ts
+++ src/features/users/reducer/user.reducer.ts
@@ -2,24 +2,37 @@ import { createReducer } from '@reduxjs/toolkit';
 import { IUser } from '../entities/users';
 import * as ac from './user.action.creator';
 
-const initialUserState: Array<IUser> = [];
+const initialUserState: {
+    isLogged: boolean;
+    isLogging: boolean;
+    user: IUser | null;
+    token: string | null;
+} = { isLogged: false, isLogging: false, user: null, token: null };
 
 export const userReducer = createReducer(initialUserState, (builder) => {
-    builder.addCase(
-        ac.userLoadActionCreator,
-        (_state, action) => action.payload
-    );
-    builder.addCase(ac.userAddActionCreator, (state, action) => [
+    builder.addCase(ac.startLoggingActionCreator, (state, _action) => ({
         ...state,
-        action.payload,
-    ]);
-    builder.addCase(ac.userUpdateActionCreator, (state, action) =>
-        state.map((item) =>
-            item.id === action.payload.id ? action.payload : item
-        )
-    );
-    builder.addCase(ac.userDeleteActionCreator, (state, action) =>
-        state.filter((item) => item.id !== action.payload.id)
-    );
+        isLogging: true,
+        isLogged: false,
+        user: null,
+        token: null,
+    }));
+    builder.addCase(ac.loginActionCreator, (state, action) => ({
+        ...state,
+        isLogging: false,
+        isLogged: true,
+        user: action.payload.user,
+        token: action.payload.token,
+    }));
+    builder.addCase(ac.logoutActionCreator, (_state, action) => action.payload);
+    builder.addCase(ac.addFavActionCreator, (state, action) => ({
+        ...state,
+        user: action.payload,
+        //users: state.user?.favPlaces
+    }));
+    builder.addCase(ac.deleteFavActionCreator, (state, action) => ({
+        ...state,
+        user: action.payload,
+    }));
     builder.addDefaultCase((state) => state);
 });
