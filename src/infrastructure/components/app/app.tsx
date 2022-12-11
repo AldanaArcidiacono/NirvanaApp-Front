cb4c0642ddbd55c0c9ac7c6a5cf8e31e358f6d5e
AldanaArcidiacono <aldana.arcidiacono@gmail.com>
2022-12-11 21:49:36 +0100

    ⚗️ Trying to conserve the token on state

--- src/infrastructure/components/app/app.tsx
+++ src/infrastructure/components/app/app.tsx
@@ -2,6 +2,7 @@ import { AppRoutes } from '../app.routes/app.routes';
 import { Layout } from '../layout/layout';
 
 export function App() {
+    //localStorage.getItem('token');
     return (
         <>
             <Layout>
