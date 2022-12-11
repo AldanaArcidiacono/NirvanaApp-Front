1d5c5b2fc35322c18b69168479acc6faeee43104
AldanaArcidiacono <aldana.arcidiacono@gmail.com>
2022-12-11 21:47:12 +0100

    💄 Add classes for styles

--- src/features/places/components/places.list/places.list.tsx
+++ src/features/places/components/places.list/places.list.tsx
@@ -1,13 +1,14 @@
 import { Link } from 'react-router-dom';
 import { IPlace } from '../../entities/places';
 import { PlacesItem } from '../places.item/places.item';
+import styles from './place.list.module.css';
 
 export function PlacesList({ item }: { item: Array<IPlace> }) {
     return (
-        <div>
-            <ul>
+        <div className={styles.list__container}>
+            <ul className={styles.list__ul}>
                 {item.map((item: IPlace) => (
-                    <li key={item.id}>
+                    <li className={styles.list__li} key={item.id}>
                         <Link to={'/details/' + item.id} key={item.id}>
                             <PlacesItem item={item} />
                         </Link>
--- src/infrastructure/components/layout/layout.tsx
+++ src/infrastructure/components/layout/layout.tsx
@@ -1,11 +1,12 @@
 import { Footer } from '../footer/footer';
 import { Header } from '../header/header';
+import style from './layout.module.css';
 
 export function Layout({ children }: { children: JSX.Element }) {
     return (
         <>
             <Header></Header>
-            {children}
+            <main className={style.main}>{children}</main>
             <Footer></Footer>
         </>
     );
